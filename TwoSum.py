e1ced4b353aba970cc22e5b19352e1070691eb99
Dev <92599559+ASCIIdot@users.noreply.github.com>
2022-11-12 09:20:05 +0000

    Create TwoSum.py

---
+++ TwoSum.py
@@ -0,0 +1,10 @@
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        for x in range(0,len(nums)):
+            for y in range(0,len(nums)):
+                if nums[x] + nums[y]== target and x!=y:
+                    returnList = [x,y]
+                    return(returnList)
+
+                else:
+                    next
